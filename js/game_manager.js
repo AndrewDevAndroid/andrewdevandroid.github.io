794e0939d058abfd8f749c0c073bc29d172e0e40
Andrei Syrakvash <andrei.syrakvash@p9ft.com>
2016-03-19 14:54:21 +0300

    Changed imaversion of generate new numbers in cells and added 8 to generate

--- js/game_manager.js
+++ js/game_manager.js
@@ -68,7 +68,8 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    rand = Math.random();
+    var value = rand < 0.8 ? 2 : (rand < 0.96 ? 4 : 8);
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
